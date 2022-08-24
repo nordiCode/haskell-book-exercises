802d35474d2ae9c0e04e069a9a228844614e9c0a
nordiCode.github.io <grantsbfi@gmail.com>
2022-08-23 20:36:05 -0700

    chapter 3 and starting 4

---
+++ finding_success/chapter_3_4/validation-book/src/Main.hs
@@ -0,0 +1,41 @@
+module Main (main) where
+import Data.Char
+
+-- Ex 7 Minimum Length
+checkPasswordLength :: String -> Maybe String
+checkPasswordLength password =
+  case (length password > 20 || length password < 5) of
+    True -> Nothing
+    False -> Just password
+
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum xs = case all isAlphaNum xs of
+  False -> Nothing
+  True -> Just xs
+
+-- only leading whitespace??
+cleanWhitespace :: String -> Maybe String
+cleanWhitespace "" = Nothing
+cleanWhitespace (x:xs) =
+  case isSpace x of
+    True -> cleanWhitespace xs 
+    False -> Just (x : xs)
+    
+-- EX.8 Combine them like previous chapter
+ultimatePassword :: String -> Maybe String
+ultimatePassword xs = case cleanWhitespace xs of
+  Nothing -> Nothing
+  Just ys -> case requireAlphaNum ys of
+    Nothing -> Nothing
+    Just zs -> case checkPasswordLength zs of
+      Nothing -> Nothing
+      Just ts -> Just ts
+  
+
+-- can run with stack repl
+main :: IO ()
+main = do
+  putStrLn "Please enter a password"
+  password <- getLine
+  print $ requireAlphaNum password
