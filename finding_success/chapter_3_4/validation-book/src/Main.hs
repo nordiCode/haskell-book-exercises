640734b4ec0385eb47359fe72ac20f4d37e62630
nordiCode.github.io <grantsbfi@gmail.com>
2022-08-24 20:12:32 -0700

    Chapter 5 exercises are in ch.3 and ch.4

--- finding_success/chapter_3_4/validation-book/src/Main.hs
+++ finding_success/chapter_3_4/validation-book/src/Main.hs
@@ -1,40 +1,40 @@
 module Main (main) where
 import Data.Char
-import InteractiveEval (Term(val))
-import Data.Text.Internal.Encoding.Utf32 (validate)
+
 
 -- Ex 7 Minimum Length
-checkPasswordLength :: String -> Maybe String
-checkPasswordLength password =
-  case (length password > 20 || length password < 5) of
-    True -> Nothing
-    False -> Just password
+checkPasswordLength :: String -> Either String String
+checkPasswordLength password = 
+  case (length password > 20) of 
+    True -> Left "Your password is too long"
+    False -> Right password
 
-
-requireAlphaNum :: String -> Maybe String
+-- Chapter 5 Exercises 
+-- Changing to Either from Maybe for error messages
+requireAlphaNum :: String -> Either String String
 requireAlphaNum xs = case all isAlphaNum xs of
-  False -> Nothing
-  True -> Just xs
+  False -> Left "Not all alphanumeric"
+  True -> Right xs
 
 -- only leading whitespace??
-cleanWhitespace :: String -> Maybe String
-cleanWhitespace "" = Nothing
+cleanWhitespace :: String -> Either String String
+cleanWhitespace "" = Left "Space"
 cleanWhitespace (x:xs) =
   case isSpace x of
     True -> cleanWhitespace xs 
-    False -> Just (x : xs)
+    False -> Right (x : xs)
     
 -- EX.8 Combine them like previous chapter
-ultimatePassword :: String -> Maybe String
-ultimatePassword xs = case cleanWhitespace xs of
-  Nothing -> Nothing
-  Just ys -> case requireAlphaNum ys of
-    Nothing -> Nothing
-    Just zs -> case checkPasswordLength zs of
-      Nothing -> Nothing
-      Just ts -> Just ts
+-- ultimatePassword :: String -> Maybe String
+-- ultimatePassword xs = case cleanWhitespace xs of
+--   Nothing -> Nothing
+--   Just ys -> case requireAlphaNum ys of
+--     Nothing -> Nothing
+--     Just zs -> case checkPasswordLength zs of
+--       Nothing -> Nothing
+--       Just ts -> Just ts
   
-validatePassword :: String -> Maybe String
+validatePassword :: String -> Either String String
 validatePassword password =
   cleanWhitespace password 
     >>= requireAlphaNum
