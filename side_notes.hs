58ffbae4743855e3d4149d74d45698fbcd7969d9
nordiCode.github.io <grantsbfi@gmail.com>
2022-08-28 19:23:59 -0700

    Do notation and bind

---
+++ side_notes.hs
@@ -0,0 +1,51 @@
+module Main where
+
+-- set builder notation
+
+setPairs :: [(Int, Int)]
+setPairs = [(x, y) | x <- [1, 2, 3], y <- [1, 2, 3], x /= y]
+
+-- monadic do notation of the set
+
+setDo :: [(Integer, Integer)]
+setDo = do
+  x <- [1, 2, 3]
+  y <- [1, 2, 3]
+  True <- return (x /= y)
+  return (x, y)
+
+-- True is a set requirment
+
+setDouble :: [(Integer, Integer)]
+setDouble = do
+  x <- [1 .. 4]
+  y <- [1 .. 4]
+  True <- return (x <= y)
+  return (2 * x, 2 * y)
+
+getPassword :: IO String
+getPassword = do
+  xs <- getLine
+  return xs
+
+doubleInt :: IO ()
+doubleInt = do
+  putStrLn "Enter a number"
+  s <- getLine
+  let num = (read s :: Int) * 2
+  putStrLn $ "Your number doubled = " <> show num
+
+doubleList :: IO ()
+doubleList = do
+  putStrLn "Enter first number"
+  s <- getLine
+  putStrLn "Enter number greater than first"
+  s2 <- getLine
+  let numList = [minS .. maxS] where
+      minS = (read s :: Int)
+      maxS = (read s2 :: Int)
+  putStrLn $ "Your list " <> show numList
+
+main :: IO ()
+main = do
+  doubleList
