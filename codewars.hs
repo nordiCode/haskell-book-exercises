4fd27054f43d03fff7b33adba355ecacf8009267
nordiCode.github.io <grantsbfi@gmail.com>
2022-10-16 13:34:27 -0700

    More codewars

--- codewars.hs
+++ codewars.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char as C
+import Data.List
 
 safeHead :: [Char] -> Char
 safeHead [] = ' '
@@ -25,13 +26,50 @@ correct' x
   | x == '0' = 'O'
   | x == '1' = 'I'
   | otherwise = x
+  
+-- feast :: String -> String -> Bool
+-- feast xs ys = s == t
+--   where
+--     s = firstLast 
+--     t = firstLast
 
-feast :: String -> String -> Bool
-feast xs ys = s == t
-  where
-    s = firstLast xs
-    t = firstLast ys
+noWhiteSpace = map (dropWhile  C.isSpace)
+
+search :: Int -> [Int] -> String
+search budget prices = intercalate "," $ map (show ) $ sort $ filter (<= budget) prices
+-- search budget prices = intersperse ',' .  $ map ( dropWhile (C.isSpace )) $ sort $ filter (<budget) prices
+
+removeSpace = dropWhile (\x -> x == ' ' )
+
+
+-- series x
+--   | x == 0 = 0
+--   | otherwise = 1 + sum [1/(4 + y)  | y <- take t $ multiples 3]
+--   where
+--     t = round x :: Int
+
+multiples :: Int -> [Int]
+multiples n = [x | x <- [0..], x `mod` n == 0]
+
+
+--  1^n / 3n-2
+
+
+-- seriesThree = [1^n / (3* n-2 ) | n <- [0..]]
+
+
+seqEq :: [Double]
+seqEq = map (1/) [1,4 ..]
+
+isSortedAndHow :: [Integer] -> String
+isSortedAndHow lst 
+  | sort lst == lst = "yes, ascending"
+  | reverse  (sort lst) == lst = "yes, descending"
+  | otherwise = "no"
 
 main :: IO ()
 main = do
-  print $ correct "ERNE5T HEM1NGWAY - A FARWELL T0 ARM5"
+  -- print $ isSortedAndHow [9,7,4,1]
+  print $ take 5 $ multiples 2
+  print $ take 5 seqEq
+  -- print $ take 5 seriesThree
