b69e2af358812ae8b3c439608484c1c11a6f4ba6
nordiCode.github.io <grantsbfi@gmail.com>
2023-04-01 14:33:25 -0700

    adding date to report

--- gitlog_local.hs
+++ gitlog_local.hs
@@ -1,17 +1,48 @@
+-- {-# OPTIONS_GHC -package old-locale #-}
 module Main where
 
 import Data.List
 import System.Process
+import Data.Time.Clock
+import Data.Time.Format
+import Data.Time.Calendar
 
--- message to make commit
-main :: IO ()
-main = do
-  output <- readProcess "git" ["log", "--format=%ad %s"] ""
-  let entries = map parseEntry $ lines output
-      grouped = groupBy (\x y -> fst x == fst y) entries
-  mapM_ print grouped
+
 
 parseEntry :: String -> (String, String)
 parseEntry line = case words line of
   (date:message) -> (date, unwords message)
   _ -> error $ "Invalid line: " ++ line
+
+formatGroupedEntries :: [[(String, String)]] -> String
+formatGroupedEntries grouped = unlines $ map formatGroup grouped
+
+formatGroup :: [(String, String)] -> String
+formatGroup entries@(x:xs) =
+  "## " ++ fst x ++ "\n\n" ++ "- " ++ unwords (map snd entries) ++ "\n"
+formatGroup [] = error "Empty group"
+
+reportISODate :: IO String
+reportISODate = do
+  currentTime <- getCurrentTime
+  let sevenDaysAgo = addDays (-7) (utctDay currentTime)
+      (year, month, day) = toGregorian sevenDaysAgo
+      isoDateString = formatTime defaultTimeLocale "%Y-%m-%d" sevenDaysAgo
+  return isoDateString
+ 
+
+main :: IO ()
+main = do
+  let filename = "test_output.md"
+    -- , "--since='1 week ago'"  add for just the one week of commits
+  output <- readProcess "git" ["log", "--format=%ad %s", "--date=short"] ""
+  let entries = map parseEntry $ lines output
+      grouped = groupBy (\x y -> fst x == fst y) entries
+      formatted = formatGroupedEntries grouped
+
+  print formatted
+  let test_output =  formatted
+  reportDate <- reportISODate
+--   let test_output = concat grouped
+  writeFile filename ("## Grant's Report for Week of "<>reportDate <> "\n" <> test_output)
+
---
+++ test_output.md
@@ -0,0 +1,49 @@
+## Grant's Report for Week of 2023-03-25
+## 2023-04-01
+
+- Commit to test Grouping git log tool
+
+## 2023-01-07
+
+- book work and codewars
+
+## 2022-12-13
+
+- learn cabal w/caesar
+
+## 2022-10-16
+
+- More codewars
+
+## 2022-10-10
+
+- codewars practice
+
+## 2022-09-20
+
+- caesar fun
+
+## 2022-08-28
+
+- Do notation and bind
+
+## 2022-08-25
+
+- starting newtype ch.6
+
+## 2022-08-24
+
+- Chapter 5 exercises are in ch.3 and ch.4
+
+## 2022-08-23
+
+- Finished chapter 4 removed dir chapter 3 and starting 4
+
+## 2022-08-22
+
+- create chapter 3 stack
+
+## 2022-08-21
+
+- Chapter two notes and sols chapter 1 notes and solutions Initial commit Initial commit
+
